bedfe46905db3507cd471f00af8f958b4b693e15
Paulo Almeida <paulo.roberto.a@hotmail.com>
2023-03-25 12:54:45 +0100

    Creating the Feed with mocked data

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
 import { useRef, useState } from "react";
-
 import useIsInViewport from "../components/useIsInViewport";
+import newsData from '../mocked_data/data';
 
-
-
-const NewsCard: React.FC = ({ index, lastNewsIndex, getNews }) => {
+const NewsCard: React.FC = ({ index }) => {
+  const newsItem = newsData[index - 1]; // Subtract 1 to match array index
   const elementRef = useRef();
   const isInViewport = useIsInViewport(elementRef);
-  const [loadNewNewsAt, setloadNewNewsAt] = useState(lastNewsIndex);
 
-  if (isInViewport) {
-    if (loadNewNewsAt === Number(elementRef.current.id)) {
-      // increase loadNewNewsAt by 2
-      setloadNewNewsAt((prev) => prev + 2);
-      getNews(3);
-    }
-  }
-
-  return <div className="slider-children">
-    <div
-      ref={elementRef}
-      id={index}
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-        height: "100%",
-      }}
-    >
-      <h1>News {index}</h1>
+  return (
+    <div className="slider-children">
+      <div
+        ref={elementRef}
+        id={index}
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          height: "100%",
+        }}
+      >
+        {newsItem ? (
+          <div>
+            <h1>{newsItem.headline}</h1>
+            <ul>
+              {newsItem.bulletPoints.map((point, i) => (
+                <li key={i}>{point}</li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
     </div>
-  </div>
+  );
+
 }
 
 export default NewsCard;
---
+++ src/mocked_data/data.js
@@ -0,0 +1,49 @@
+const newsData = [
+  {
+    id: 1,
+    headline: "Apple announces new iPhone 14 series",
+    bulletPoints: [
+      "New A16 chip delivers faster performance",
+      "Improved battery life and camera capabilities",
+      "Available in four different models",
+    ],
+  },
+  {
+    id: 2,
+    headline: "Tesla unveils electric semi-truck",
+    bulletPoints: [
+      "Range of up to 500 miles on a single charge",
+      "Enhanced Autopilot features for highway driving",
+      "Production starting in 2023",
+    ],
+  },
+  {
+    id: 3,
+    headline: "Microsoft acquires AI startup DeepLearn",
+    bulletPoints: [
+      "DeepLearn specializes in deep learning algorithms",
+      "Acquisition expected to enhance Azure AI capabilities",
+      "Deal valued at $500 million",
+    ],
+  },
+  {
+    id: 4,
+    headline: "Google releases Android 14 with new privacy features",
+    bulletPoints: [
+      "App hibernation to save battery life and storage",
+      "Improved permission handling and user control",
+      "New design language called Material NEXT",
+    ],
+  },
+  {
+    id: 5,
+    headline: "Amazon launches new Alexa-powered smart home devices",
+    bulletPoints: [
+      "Expanded range includes smart speakers, displays, and cameras",
+      "Improved voice recognition and natural language processing",
+      "Integration with third-party services and platforms",
+    ],
+  },
+];
+
+export default newsData;
