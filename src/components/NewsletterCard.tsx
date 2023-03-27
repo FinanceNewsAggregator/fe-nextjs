1bbbfe51c978fce7f4a5933e5e71eb6bca561047
Paulo Almeida <paulo.roberto.a@hotmail.com>
2023-03-27 21:34:54 +0200

    Newsletter-feed done

--- src/components/NewsletterCard.tsx
+++ src/components/NewsletterCard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const NewsletterCard = ({ newsletter }) => {
   return (
-    <div className="rounded-lg shadow-lg bg-white p-4">
+    <div className="rounded-lg bg-white p-4">
       <h3 className="text-lg font-semibold mb-2">{newsletter.title}</h3>
       <p className="text-sm text-gray-600">{newsletter.content}</p>
       <div className="mt-4">
