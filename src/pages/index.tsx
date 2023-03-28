6278cad3b8b7ff1834283bb5fe6f7935304a3b87
Paulo Almeida <paulo.roberto.a@hotmail.com>
2023-03-28 15:58:10 +0200

    improving the visualization & make it looks like a mobile phone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,42 +64,44 @@ export default function HomePage() {
       <Seo />
 
       <main className="text-black">
-        <div class="flex-col h-screen items-center justify-center bg-indigo-50 px-4">
-          <div className="pt-16 pb-4 flex flex-col items-center">
-            <h1 className="text-4xl font-semibold mb-2 ">Dripper News</h1>
-            <h2 className="text-2xl font-medium">Latest News</h2>
-          </div>
-          <div className="flex justify-center">
-            <div className="w-96 overflow-hidden rounded-xl bg-white p-5">
-              <ReactAnimatedProgressBar
-                width="100%"
-                height="5px"
-                rect
-                fontColor="black"
-                percentage={`${(10 - counter) * 10}`}
-                rectPadding="1px"
-                rectBorderRadius="20px"
-                trackPathColor="transparent"
-                bgColor="#e6e6e6"
-                trackBorderColor="#e6e6e6"
-                trackBorderWidth="1px"
-                trackBorderRadius="20px"
-                trackColor="#3a3a3a"
-                trackWidth="100%"
-                trackRemainingPathColor="#e6e6e6"
-                pointerRadius="0px"
-                pointerColor="#3a3a3a"
-                pointerBorderWidth="0px"
-                pointerBorderColor="transparent"
-              />
-              <CombinedCard item={combinedData[currentIndex]} />
-              <div className="mt-3 flex space-x-4">
-                <button onClick={() => handleButtonClick("previous")} className="w-full rounded-md bg-black py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">Previous</button>
-                <button onClick={() => handleButtonClick("next")} className="w-full rounded-md bg-black py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">Next</button>
-              </div>
-              <div className="mt-3 flex-col space-x-4">
-                <p className="my-2">Changing content in {counter} seconds</p>
-                <BottomNav scrollNews={undefined} />
+        <div className="flex-col h-screen items-center justify-center bg-indigo-50 px-4">
+          <div className="mx-auto mb-5 max-w-sm flex-col rounded-3xl border-4 border-t-8 border-gray-900 bg-white  py-6 text-black shadow-2xl">
+            <div className="flex justify-center">
+              <div className="w-96 overflow-hidden rounded-xl bg-white p-5">
+                <div className="pt-4 pb-4 flex flex-col items-center">
+                  <h1 className="text-4xl font-semibold mb-2 ">Dripper News</h1>
+                  <h2 className="text-2xl font-medium">Latest News</h2>
+                </div>
+                <ReactAnimatedProgressBar
+                  width="100%"
+                  height="5px"
+                  rect
+                  fontColor="black"
+                  percentage={`${(10 - counter) * 10}`}
+                  rectPadding="1px"
+                  rectBorderRadius="20px"
+                  trackPathColor="transparent"
+                  bgColor="#e6e6e6"
+                  trackBorderColor="#e6e6e6"
+                  trackBorderWidth="1px"
+                  trackBorderRadius="20px"
+                  trackColor="#3a3a3a"
+                  trackWidth="100%"
+                  trackRemainingPathColor="#e6e6e6"
+                  pointerRadius="0px"
+                  pointerColor="#3a3a3a"
+                  pointerBorderWidth="0px"
+                  pointerBorderColor="transparent"
+                />
+                <CombinedCard item={combinedData[currentIndex]} />
+                <div className="mt-3 flex space-x-4">
+                  <button onClick={() => handleButtonClick("previous")} className="w-full rounded-md bg-black py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">Previous</button>
+                  <button onClick={() => handleButtonClick("next")} className="w-full rounded-md bg-black py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">Next</button>
+                </div>
+                <div className="mt-3 flex-col space-x-4">
+                  <p className="my-2">Changing content in {counter} seconds</p>
+                  <BottomNav scrollNews={undefined} />
+                </div>
               </div>
             </div>
           </div>
